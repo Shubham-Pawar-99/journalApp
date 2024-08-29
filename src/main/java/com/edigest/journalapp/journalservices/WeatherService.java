40deb10560aae86bbe6c1e947429ae43f9ecece4
Shubham-Pawar-99 <159262602+Shubham-Pawar-99@users.noreply.github.com>
2024-08-29 19:32:29 +0530

    External Api

---
+++ src/main/java/com/edigest/journalapp/journalservices/WeatherService.java
@@ -0,0 +1,12 @@
+package com.edigest.journalapp.journalservices;
+
+public class WeatherService {
+
+    private static final String api_key = "c350d1c54d7a0a5bef4f45fe69d6d3b6";
+
+    public String getWeather(String city){
+        return null;
+    }
+
+
+}
